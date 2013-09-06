a9b3fbee4b97df18a5b17982485cf879b3e92aa3
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 14:29:21 -0500

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,25 +6,24 @@ class User < ActiveRecord::Base
 
   has_many :responses
   has_many :surveys
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :email, presence: true
-  validates :name, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
 
-  def self.authenticate(name, secret)
-    user = find_by_name(name)
-    if user && user.password == secret
-      return user
-    end
-  end
+  # def self.authenticate(name, secret)
+  #   user = find_by_name(name)
+  #   if user && user.password == secret
+  #     return user
+  #   end
+  # end
 
-  def password
-    @password = Password.new(password_digest)
-  end
+  # def password
+  #   @password = Password.new(password_digest)
+  # end
 
-  def password=(secret)
-    @password = Password.create(secret)
-    self.password_digest = @password
-  end
+  # def password=(secret)
+  #   @password = Password.create(secret)
+  #   self.password_digest = @password
+  # end
 end
