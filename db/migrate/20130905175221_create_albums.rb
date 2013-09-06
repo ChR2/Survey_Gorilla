53ebd9153bf34daa6c353bc73c8dac45735967a6
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 13:00:50 -0500

    .

--- db/migrate/20130905175221_create_albums.rb
+++
@@ -1,8 +0,0 @@
-class CreateAlbums < ActiveRecord::Migration
-  def change
-  	create_table :albums do |t|
-  		t.belongs_to :user
-  		t.string :name
-  	end
-  end
-end
--- db/migrate/20130905175226_create_photos.rb
+++
@@ -1,9 +0,0 @@
-class CreatePhotos < ActiveRecord::Migration
-  def change
-  	create_table :photos do |t|
-  		t.belongs_to :album
-  		t.string :name
-  		t.string :file
-  	end
-  end
-end
